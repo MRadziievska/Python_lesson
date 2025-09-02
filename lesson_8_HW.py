237b679f17d5edf60975a7b6b6dd7e1a26a2e441
MRadziievska <150276032+MRadziievska@users.noreply.github.com>
2025-09-02 16:50:38 +0300

    Add files via upload

---
+++ lesson_8_HW.py
@@ -0,0 +1,23 @@
+#1
+# text = input("Text: ")
+# text = text.upper()
+#
+# print(text)
+
+#2
+# text = input("Text: ")
+#
+# if "a" in text:
+#     mod_text = text.replace("a", "*")
+#     print("результат: ", mod_text)
+# else:
+#     print("нет 'а'")
+
+#3
+bukvi = input("Text: ")
+n_bukvi = len(bukvi)
+
+print(n_bukvi)
+
+
+
