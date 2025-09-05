7323eb9786e8ba6ecf373ecce627ccb2fa5ee4dd
MRadziievska <150276032+MRadziievska@users.noreply.github.com>
2025-09-05 11:19:58 +0300

    Add files via upload

---
+++ lesson_9_HW.py
@@ -0,0 +1,36 @@
+#1
+# city = "Odessa"
+# temperature = 32
+#
+# concatenate_string = "The temperature in the city " + city + " right now is " + str(temperature) + " degrees Celsius."
+# print(concatenate_string)
+#
+# f_string = f"The temperature in the city {city} right now is {temperature} degrees Celsius."
+# print(f_string)
+
+#2
+# name = input("Укажите имя: ")
+# age = input("Укажите возраст: ")
+# city = input("Укажите место проживания: ")
+#
+# f_stringe_1 = f"Name: {name}"
+# f_stringe_2= f"Age: {age}"
+# f_stringe_3 = f"Place of residence: {city}"
+# print(f_stringe_1)
+# print(f_stringe_2)
+# print(f_stringe_3)
+
+#3
+lenght = float(input("Укажите длину: "))
+width = float(input("Укажите ширину: "))
+
+f_stringe_1 = f"Долина= {lenght}"
+f_stringe_2= f"Ширина= {width}"
+
+P = 2 * (lenght + width)
+S = lenght * width
+
+print(f_stringe_1)
+print(f_stringe_2)
+print("P = ", P)
+print("S = ", S)
