03e8ae485ce2aabd418c394c843f09369250ba49
MRadziievska <150276032+MRadziievska@users.noreply.github.com>
2025-09-10 10:54:23 +0300

    Add files via upload

---
+++ lesson_10_HW.py
@@ -0,0 +1,14 @@
+#1
+# my_tuple_1 = (1, 2, 3)
+# print(my_tuple_1[0])
+# print(my_tuple_1[2])
+
+#2
+# my_tuple_2 = (1, 2, 3)
+# for i in my_tuple_2:
+#     print(i)
+
+#3
+my_tuple_3 = (1, 2, 3)
+print(min(my_tuple_3))
+print(max(my_tuple_3))
