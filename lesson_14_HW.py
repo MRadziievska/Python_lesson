28c31f79f507a5dd971803aa4b12871f7a5212e3
MRadziievska <150276032+MRadziievska@users.noreply.github.com>
2025-09-11 14:59:27 +0300

    Add files via upload

---
+++ lesson_14_HW.py
@@ -0,0 +1,18 @@
+#1
+# for i in range(1, 11):
+#     print(f"=={i}==")
+#     for j in range(2, 10):
+#         print(f"{i}*{j}={i*j}")
+
+#2
+# for i in range(1, 11):
+#     print(i**2)
+
+#3
+sum = 0
+for i in range(1, 101):
+    sum += i
+print(i)
+
+
+
