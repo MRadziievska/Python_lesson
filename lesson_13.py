20c9bccd725f0b1801238192e3de2388dc2eb862
MRadziievska <150276032+MRadziievska@users.noreply.github.com>
2025-09-11 11:31:39 +0300

    Add files via upload

---
+++ lesson_13.py
@@ -0,0 +1,15 @@
+# total_adds = 92
+# adds_per_pages = 8
+# full_pages_count = total_adds // adds_per_pages
+# print(full_pages_count)
+# last_page_adds = total_adds % adds_per_pages
+# print(last_page_adds)
+
+total_apples = 11
+total_person = 3
+apples_per_person = total_apples // total_person
+print(apples_per_person)
+remaining_apples = total_apples % total_person
+print(remaining_apples)
+
+
---
+++ lesson_13_HW.py
@@ -0,0 +1,30 @@
+#1
+# total_task = 77
+# total_person = 13
+#
+# task_per_person = total_task // total_person
+# print(task_per_person)
+#
+# ramaining_task = total_task % total_person
+# print(ramaining_task)
+
+#2
+# photo = 168
+# page = 8
+#
+# total_pages = photo // page
+# print(total_pages)
+#
+# ramaining_photo = photo % page
+# print(ramaining_photo)
+
+#3
+total_price = 45999
+month_price = 7500
+
+total_months = total_price // month_price
+ramaining_price = total_price % month_price
+
+print(f"Кол-во выплат: по {month_price} грн - {total_months} месяцев and 1 месяц - {ramaining_price} грн")
+
+
